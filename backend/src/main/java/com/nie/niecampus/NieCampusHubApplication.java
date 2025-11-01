c9deb3f75f8096afcd31aab4eac8a56bd377225a
TejaswiniH123 <tejaswinih9@gmail.com>
2025-11-01 13:19:06 +0530

    Initial commit

---
+++ backend/src/main/java/com/nie/niecampus/NieCampusHubApplication.java
@@ -0,0 +1,13 @@
+package com.nie.niecampus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NieCampusHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NieCampusHubApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/nie/niecampus/NieCampusHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nie.niecampus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NieCampusHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
